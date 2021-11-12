5b3fd93c4476096e23c0d253980e2c0c1d472ffb
houzairkoussa <houzairmk@icloud.com>
2021-11-12 21:49:11 +0400

    refactored

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const grids = document.querySelectorAll('.grid');
 
-// * Initialise
+// * Game Initialise
 
 // ! Number Picker
 const numberPicker = (index, number) => {
@@ -14,22 +14,23 @@ let randomArray = [Math.round(Math.random() * 15), Math.round(Math.random() * 15
 while (randomArray[0] === randomArray[1]) {
   randomArray = [Math.round(Math.random() * 15), Math.round(Math.random() * 15)]
 }
-randomArray.forEach((random) => {
-  numberPicker(random, 2);
-});
+randomArray.forEach(random => numberPicker(random, 2));
 
 
+// * Game Play
 
+// ! Flow Checkers
+const arrowUp = () => {} // TODO
+const arrowDown = () => {} // TODO
+const arrowLeft = () => {} // TODO
+const arrowRight = () => {} // TODO
+
+// ! Flow Control
 const game = (key) => {
-  if (key === "ArrowUp") {
-
-  } else if (key === "ArrowDown") {
-
-  } else if (key === "ArrowLeft") {
-
-  } else if (key === "ArrowRight") {
-
-  }
+  if (key === "ArrowUp") arrowUp();
+  if (key === "ArrowDown") arrowDown();
+  if (key === "ArrowLeft") arrowLeft();
+  if (key === "ArrowRight") arrowRight();
 }
 
 window.addEventListener('keyup', event => game(event.key));
